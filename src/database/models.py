40dda119d0651b13f2c6758466dd65ff97211583
Rafal Pietras <rafal.radx@gmail.com>
2024-04-28 01:02:23 +0200

    manage script for adding users to db outside API

--- src/database/models.py
+++ src/database/models.py
@@ -53,7 +53,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
+    username = Column(String(50), nullable=False)
     password = Column(String(128), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     role = Column(
