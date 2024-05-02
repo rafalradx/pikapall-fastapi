7b2ed6592f7b68aa60058dc981066b8ac272a693
kachnna <137805850+kachnna@users.noreply.github.com>
2024-05-02 17:34:12 +0200

    tests

--- tests/test_unit_repository_ratings.py
+++ tests/test_unit_repository_ratings.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class TestRatings(unittest.IsolatedAsyncioTestCase):
 
-    def setUp(self):
+    async def asyncSetUp(self):
         self.db_session = MagicMock()
         self.rating_repository = RatingRepository(db_session=self.db_session)
 
@@ -24,14 +24,13 @@ class TestRatings(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(created_rating.photo_id, test_photo_id)
         self.assertEqual(created_rating.user_id, test_user_id)
         self.assertEqual(created_rating.rating, test_rating)
-        self.assertIsInstance(created_rating.created_at, datetime)
-        self.assertIsInstance(created_rating.updated_at, datetime)
 
-    async def test_get_all_ratings(self):
-        mock_ratings = [Rating(id=1, photo_id=1, user_id=1, rating=4), Rating(
-            id=2, photo_id=2, user_id=2, rating=3)]
-        self.db_session.query.return_value.offset.return_value.limit.return_value.all.return_value = mock_ratings
-        result = await self.rating_repository.get_all_ratings(skip=0, limit=10)
+    async def test_get_ratings(self):
+        mock_rating_1 = Rating(id=1, photo_id=1, user_id=1, rating=4)
+        mock_rating_2 = Rating(id=2, photo_id=2, user_id=2, rating=3)
+        mock_ratings = [mock_rating_1, mock_rating_2]
+        self.db_session.query.return_value.filter.return_value.all.return_value = mock_ratings
+        result = await self.rating_repository.get_ratings()
         self.assertEqual(result, mock_ratings)
 
     async def test_get_rating_by_id(self):
@@ -41,6 +40,12 @@ class TestRatings(unittest.IsolatedAsyncioTestCase):
         self.db_session.query.return_value.filter.return_value.first.return_value = mock_rating
         result = await self.rating_repository.get_rating_by_id(rating_id=test_rating_id)
         self.assertEqual(result, mock_rating)
+
+    async def test_get_rating_by_id_not_found(self):
+        test_rating_id = 999
+        self.db_session.query.return_value.filter.return_value.first.return_value = None
+        result = await self.rating_repository.get_rating_by_id(rating_id=test_rating_id)
+        self.assertIsNone(result)
 
 
 if __name__ == "__main__":
