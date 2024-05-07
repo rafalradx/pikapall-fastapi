9256a7a3890a4eaaa1dca885119cf1ada3496df1
Rafal Pietras <rafal.radx@gmail.com>
2024-05-08 00:05:29 +0200

    db print removed

--- src/database/db.py
+++ src/database/db.py
@@ -11,7 +11,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # db session with automatic closing
 def get_db():
     db = SessionLocal()
-    print("DB read")
     try:
         yield db
     except Exception as err:
