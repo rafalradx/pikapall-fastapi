13ecbcebca4d44b804e97fc57795d4b11dfd77aa
Rafal Pietras <rafal.radx@gmail.com>
2024-05-07 20:15:17 +0200

    db loging for debug

--- src/database/db.py
+++ src/database/db.py
@@ -4,7 +4,7 @@ from src.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo_pool=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -13,5 +13,9 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception as err:
+        # log your error
+        print(err)  # for dev/testing
+        db.rollback()
     finally:
         db.close()
